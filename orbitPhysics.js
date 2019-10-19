210efda972a621c2966f0a0d982129f50b36a8c8
andrew <aargraves@berkeley.edu>
2019-10-19 19:55:27 -0400

    Got frames + json array mapped out array procedure

--- orbitPhysics.js
+++ orbitPhysics.js
@@ -1,10 +1,10 @@
 var dummy_array = [{
-    "start_x": 1000,
-    "start_y": 1000,
-    "x_velocity":10000,
-    "y_velocity": 10000,
-    "planet_mass": 1000,
-    "sun_mass": 100000000000000
+    "start_x": 149.6 * Math.pow(10, 9),           //earths km from the sun
+    "start_y": 0,
+    "x_velocity":0,
+    "y_velocity": 30000,                         //velocity in m/s assuming the earth starts at 0
+    "planet_mass": 5.92 * Math.pow(10, 24),      //kg
+    "sun_mass": 1.989 * Math.pow(10, 30)         //kg
 }];
 
 function cheapTrick() {
@@ -17,18 +17,19 @@ function startMotion(data) {
 
     for (i=0 ; i < data.length ; i++) {
         //frame_array.shift();
-        frame_array = [(moveBody(data[i].start_x, data[i].start_y, data[i].x_velocity, data[i].y_velocity, data[i].planet_mass, data[i].sun_mass, 3))]
+        frame_array = [(moveBody(data[i].start_x, data[i].start_y, data[i].x_velocity, data[i].y_velocity, data[i].planet_mass, 86400))]
     }
     console.log(frame_array)
     return frame_array
-    //setInterval (startMotion(frame_array), 1000)
+    setInterval (startMotion(frame_array), 1000)
 }
 
-function moveBody(x, y, dx, dy, m_planet, m_sun, time) {
+function moveBody(x, y, dx, dy, m_planet, time) {
 
     var theta       =   Math.atan(x / y),
         radius      =   Math.sqrt(Math.pow(x ,2) + Math.pow(y ,2)   ),
         G           =   6.67408 * Math.pow(10, -11),
+        m_sun       =   1.989 * Math.pow(10, 30),
         force       =   (m_planet * m_sun * G )/ Math.pow(radius, 2),
         force_x     =   force * Math.cos(theta),
         force_y     =   force * Math.sin(theta),
@@ -36,8 +37,8 @@ function moveBody(x, y, dx, dy, m_planet, m_sun, time) {
         ddy         =   force_y / m_planet;
 
 
-    console.log(theta)
-    console.log(radius)
+    // console.log(theta)
+    // console.log(radius)
 
     dx = dx + ddx * time
     dy = dy + ddy * time
@@ -45,5 +46,5 @@ function moveBody(x, y, dx, dy, m_planet, m_sun, time) {
     x = x + dx * time + .5 * ddx * Math.pow(time, 2)
     y = y + dy * time + .5 * ddy * Math.pow(time, 2)
 
-    return {"start_x": x, "start_y": y, "x_velocity": dx, "y_velocity": dy, "planet_mass": m_planet, "sun_mass": m_sun, "time": time}
+    return {"start_x": x, "start_y": y, "x_velocity": dx, "y_velocity": dy, "planet_mass": m_planet, "time": time}
 };
