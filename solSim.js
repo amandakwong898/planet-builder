21a3fffa69e3b58d9bad499251f3b03bc077bda2
Melanie <mldworak@wpi.edu>
2019-10-19 19:34:37 -0400

    working radio button functions!!!!!!!!!!!!!

--- solSim.js
+++ solSim.js
@@ -1,11 +1,22 @@
-$("#starRadio").onclick(() => {
-    console.log("onclick is running");
-    $("body_setup").append(
-        " <form>\n" +
-        "            <input type = \"text\" name = \"\">Name<br>\n" +
-        "            <input type = \"text\" name = \"\">Mass<br>\n" +
-        "            <input type = \"text\" name = \"\">Radius<br>\n" +
-        "            <input type = \"text\" name = \"\">Temperature<br>\n" +
-        "        </form>\n"
-    )
-})
+// $("#starRadio").onclick(() => {
+//     console.log("onclick is running");
+//     $("body_setup").append(
+//         " <form>\n" +
+//         "            <input type = \"text\" name = \"\">Name<br>\n" +
+//         "            <input type = \"text\" name = \"\">Mass<br>\n" +
+//         "            <input type = \"text\" name = \"\">Radius<br>\n" +
+//         "            <input type = \"text\" name = \"\">Temperature<br>\n" +
+//         "</form>\n"
+//     )
+// }
+
+
+
+
+function populateStar() {
+    document.getElementById("body_setup").innerHTML = "<button> yoyoyoyoyo </button>";
+}
+
+function populatePlanet() {
+    document.getElementById("body_setup").innerHTML = "<button> biiiiiiiiiiiiiitch </button>";
+}
